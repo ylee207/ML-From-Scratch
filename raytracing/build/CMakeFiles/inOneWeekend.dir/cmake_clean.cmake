1a2aa4f12693e7edf8b6c80413cd4cbe4997bdbb
ylee207 <ylee207@jh.edu>
2024-07-27 03:25:18 -0700

    raytracer

---
+++ raytracing/build/CMakeFiles/inOneWeekend.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inOneWeekend.dir/main.cc.o"
+  "CMakeFiles/inOneWeekend.dir/main.cc.o.d"
+  "inOneWeekend"
+  "inOneWeekend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inOneWeekend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
